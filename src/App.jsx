c4cdc8c871cc2fe326c3d030b81ebec7f29b3062
tildamt <128397429+tildamt@users.noreply.github.com>
2024-02-06 17:36:36 +0200

    exercise 5.10

--- src/App.jsx
+++ src/App.jsx
@@ -130,7 +130,9 @@ const App = () => {
         <div>
           {blogForm()}
         </div>
-      {blogs.map(blog =>
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
         <Blog key={blog.id} blog={blog} user={currentUser} likeBlog={likeBlog} />
       )}
     </div>
