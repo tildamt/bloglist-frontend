f2df447a13359b24eb9ca5a05fcac91c8fae5159
tildamt <128397429+tildamt@users.noreply.github.com>
2024-02-07 18:31:01 +0200

    exercise 5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,6 @@
 describe('Blog app', function() {
   beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    //cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:5173')
   })
 
@@ -9,4 +9,23 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
   })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      /*cy.contains('login').click()*/
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('mluukkai is logged in')
+    })
+
+    it.only('fails with wrong credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen123')
+      cy.get('#login-button').click()
+
+    })
+  })
+
 })
